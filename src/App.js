66b1ef40ce20f2ba7debcda45347c51162110f25
“prudhvikov” <prudvhvi@yahoo.com>
2022-08-04 12:30:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [data, setdata] = useState({
+	address: "",
+	Balance: null,
+});
+
+const btnhandler = () => {
+
+	if (window.ethereum) {
+
+	window.ethereum
+		.request({ method: "eth_requestAccounts" })
+		.then((res) => accountChangeHandler(res[0]));
+	} else {
+	alert("install metamask extension!!");
+	}
+};
+
+
+const getbalance = (address) => {
+
+	
+	window.ethereum
+	.request({
+		method: "eth_getBalance",
+		params: [address, "latest"]
+	})
+	.then((balance) => {
+	
+		setdata({
+		Balance: ethers.utils.formatEther(balance),
+		});
+	});
+};
+
+
+const accountChangeHandler = (account) => {
+	
+	setdata({
+	address: account,
+	});
+
+	getbalance(account);
+};
+
+return (
+	<div className="App">
+
+
+	<Card className="text-center">
+		<Card.Header>
+		<strong>Address: </strong>
+		{data.address}
+		</Card.Header>
+		<Card.Body>
+		<Card.Text>
+			<strong>Balance: </strong>
+			{data.Balance}
+		</Card.Text>
+		<Button onClick={btnhandler} variant="primary">
+			Connect to wallet
+		</Button>
+		</Card.Body>
+	</Card>
+	</div>
+);
 }
 
 export default App;
