3a7be79315fc5b31ab037d4331092c8941364fe9
Prakharsharma2206 <prakharsharma22@gmail.com>
2022-08-07 01:01:02 +0530

    S final

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ function App() {
   });
 
   const [nft, setnft] = useState(null);
-  useEffect(() => {
-    console.log(nft);
-    if (nft != null) {
-      console.log(nft[0].chain);
-      console.log(nft[0].collectionAddress);
-      console.log(nft[0].collectionName);
-      console.log(nft[0].collectionTokenId);
-      console.log(nft[0].currentOwner);
-      console.log(nft[0].description);
-      console.log(nft[0].imageUrl);
-      console.log(nft[0].name);
-      console.log(nft[0].network);
-    }
-  }, [nft]);
+  // useEffect(() => {
+  //   console.log(nft);
+  //   if (nft != null) {
+  //     console.log(nft[0].chain);
+  //     console.log(nft[0].collectionAddress);
+  //     console.log(nft[0].collectionName);
+  //     console.log(nft[0].collectionTokenId);
+  //     console.log(nft[0].currentOwner);
+  //     console.log(nft[0].description);
+  //     console.log(nft[0].imageUrl);
+  //     console.log(nft[0].name);
+  //     console.log(nft[0].network);
+  //   }
+  // }, [nft]);
   return (
     <div className="App">
       {data.address === "" ? (
