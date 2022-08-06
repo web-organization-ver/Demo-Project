d39357cdf591bd3546ee79ed957ea54cd38005f2
Prakharsharma2206 <prakharsharma22@gmail.com>
2022-08-06 12:51:30 +0530

    Basic Clean

---
+++ src/cardwallet.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { ethers } from "ethers";
+
+const cardwallet = ({ data, setdata }) => {
+  const getbalance = (address) => {
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance) => {
+        setdata({
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+
+  const accountChangeHandler = (account) => {
+    setdata({
+      address: account,
+    });
+
+    getbalance(account);
+  };
+
+  const btnhandler = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+  return (
+    <div className="walletcard">
+      <Card className="text-center">
+        <Card.Header>
+          <strong>Address: </strong>
+          {data.address}
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <strong>Balance: </strong>
+            {data.Balance}
+          </Card.Text>
+          <Button onClick={btnhandler} variant="primary">
+            Connect to wallet
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default cardwallet;
